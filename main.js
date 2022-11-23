a5011c4b01598f9d8004d1eb6fd46d5a0c231a19
sraya691 <98817930+sraya691@users.noreply.github.com>
2022-11-23 17:43:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWrightX = 0;
+difference = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -18,5 +23,20 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + "difference = " + difference);
     }
+}
+
+function draw() {
+    background("hotpink");
+    fill("deeppink");
+    textSize(difference);
+    text("Shreya", noseX, noseY);
 }
